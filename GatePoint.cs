e2afb79004e92fe798ac60cec3ddb83240132aeb
Oluwabiyi (Martin) Akinsara <wabiyi.25@gmail.com>
2018-01-30 17:37:13 -0500

    Opps forgot to add this

---
+++ GatePoint.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Teleporter;
+using teleporter = Teleporter.TeleporterHelper;
+
+/// <summary>
+/// Gate point.
+/// Warp gates Teleport to it's pair 
+/// id == same id
+/// 0 Random warp
+/// </summary>
+
+[AddComponentMenu("GatePoint")]
+public class GatePoint : MonoBehaviour {
+
+	Transform pos;
+
+	public int id;
+	public List<Object> objType = new List<Object>();
+
+	// Use this for initialization
+	void Start () {
+		pos = this.gameObject.transform;
+		teleporter.Instance.warpPoints.Add(this);
+	}
+	
+	void OnCollisionEnter(Collision other){
+		if (objType != null) {
+			foreach (var obj in objType) {
+				if (other.gameObject.GetComponent (obj.name) != null) {
+					if (this.id == 0) {
+						teleporter.Instance.warpRandom (other.gameObject);
+						break;
+					}
+					teleporter.Instance.warpTo (other.gameObject, (new WarpPoint (this.id, this.pos)));
+					break;
+				}  
+			}
+		}
+	}
+
+	public Transform getPos(){
+		return pos;
+	}
+}
---
+++ TeleporterHelper.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Reflection;
+
+/// <summary>
+/// Teleporter helper.
+/// When too lazy to write simple teleporting code
+/// Pair each gateway by matching Ids.
+/// </summary>
+namespace Teleporter{
+	public class TeleporterHelper{
+
+		private static TeleporterHelper instance = null;
+		private static readonly object padlock = new object();
+
+		public static TeleporterHelper Instance{
+			get{ 
+				lock (padlock) {
+					if (instance == null) {
+						instance = new TeleporterHelper ();
+					}
+					return instance;
+				}
+			}
+		}
+
+		public List<GatePoint> warpPoints = new List<GatePoint>();
+
+		public void warpTo(GameObject obj, WarpPoint point){
+			foreach (var pt in warpPoints) {
+				int nextId = pt.id;
+				Vector3 nextPos = pt.getPos().position;
+				if (point.id == nextId && point.pos.position != nextPos) {
+					nextPos.z += 2f;
+					obj.transform.position = nextPos;
+					break;
+				}
+			}
+		}
+
+		public void warpRandom(GameObject obj){
+			int randInt = Random.Range (1, warpPoints.Count);
+			GatePoint pt = warpPoints [randInt]; 
+			Vector3 nextPos = pt.getPos ().position;
+			nextPos.z += 2f;
+			obj.transform.position = nextPos;
+		}
+	}
+
+	[System.Serializable]
+	public struct WarpPoint{
+		public int id;
+		public Transform pos;
+		public WarpPoint (int ID, Transform Pos)
+		{
+			this.id = ID;
+			this.pos = Pos;
+		}
+	}
+}
